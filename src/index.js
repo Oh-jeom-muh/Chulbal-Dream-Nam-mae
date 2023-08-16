93c64489708f085a7fd4af84c45c475656ddf9c8
shinjeoungho <hole546@naver.com>
2023-08-16 15:03:26 +0900

    찐 초기셋팅

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from 'Router';
-import "style/common.scss";
 import "style/reset.scss";
+import "style/common.scss";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
