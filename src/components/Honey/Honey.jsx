2413c16b209ec41962409dc6610008415b5f9c20
eunbinmoon <bin1109a@gmail.com>
2023-08-26 08:08:39 +0900

    꿀맛조합

--- src/components/Honey/Honey.jsx
+++ src/components/Honey/Honey.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import "./Honey.scss";
 
 const Honey = () => {
-  return <div>asd</div>;
+  const [honeyData, setHoneyData] = useState([]);
+
+  useEffect(() => {
+    fetch("/data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const honeyList = data.map((item) => item.list);
+        setHoneyData(honeyList);
+      });
+  }, []);
+  console.log(honeyData);
+
+  return (
+    <>
+      <div className="HoneyWrap">
+        <strong className="eatingTitle">꿀맛 조합</strong>
+        {honeyData.map((data, index) => (
+          <div className="honeyList" key={index}>
+            <div className="honeyBox" />
+            <p className="honeyText">{data}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Honey;
